1c7341274fa59b68b5b932bf7862719cfdb0451c
Divyank Shah <divyank.shah.2016@gmail.com>
2023-04-29 16:49:09 -0700

    fix origin

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "https://r-land.vercel.app",
+    origin: "*",
   },
 });
 
