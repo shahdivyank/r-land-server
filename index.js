ec186dc008c91f833fe1aa7676cde4b7e3e390fe
Divyank Shah <divyank.shah.2016@gmail.com>
2023-04-29 16:32:57 -0700

    finish socket

--- index.js
+++ index.js
@@ -3,31 +3,27 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+const io = new Server(server, {
+  //   cors: {
+  //     origin: "http://localhost:3000",
+  //   },
 });
 
-// io.on("connection", (socket) => {
-//   console.log("a user connected");
-//   socket.on("disconnect", () => {
-//     console.log("user disconnected");
-//   });
-// });
-
-// io.on("connection", (socket) => {
-//   socket.on("chat message", (msg) => {
-//     console.log("message: " + msg);
-//   });
-// });
-
 io.on("connection", (socket) => {
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+  socket.on("join", (room) => {
+    socket.join(room);
+  });
+
+  socket.on("leave", (room) => {
+    socket.leave(room);
+  });
+
+  socket.on("games", (msg) => {
+    console.log(socket.rooms, "GOT GAMES MESSAGE", msg);
+    socket.to("RANDOM ROOM ID").emit("games", msg);
   });
 });
 
-server.listen(3000, () => {
-  console.log("listening on *:3000");
+server.listen(8080, () => {
+  console.log("listening on *:8080");
 });
