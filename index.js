39277fc00e38ee4f2f45b0b592fb87e82f9dd9f6
Divyank Shah <divyank.shah.2016@gmail.com>
2023-04-29 11:46:34 -0700

    setup express + socket

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello world");
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
